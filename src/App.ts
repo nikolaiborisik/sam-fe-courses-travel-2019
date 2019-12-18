ed2c17bf8a7fc1b167953165739b256303fbf93e
Leonid Prusov <39962817+SpiderQshka@users.noreply.github.com>
2019-12-18 16:00:26 +0300

    styles-review-buttons

--- src/App.ts
+++ src/App.ts
@@ -1,7 +1,8 @@
 import './styles/app.sass';
 import './scripts/services.ts'
+import './components/card/card.ts'
 import './components/header/header.ts'
 import './components/main/main.ts'
-import  './components/aside/aside.ts'
+import './components/aside/aside.ts'
 import './components/gallery/gallery.ts'
 import './components/footer/footer.ts'

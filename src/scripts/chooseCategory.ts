13c40d86a25ca71624edaaecd9d961f865d75bd3
georgyshkliaryk <shkliargosh23@gmail.com>
2019-12-20 19:22:28 +0100

    images updated, clear categories, json fix

--- src/scripts/chooseCategory.ts
+++ src/scripts/chooseCategory.ts
@@ -10,35 +10,9 @@ export let currentCategory: string;
 categoryForm.addEventListener('change', getCategory);
 
 function getCategory() {
-  /*  for (let i=0; i<categoryForm.length; i++) {
-        if (categoryRadio[i].checked) {
-            categoryValue = categoryRadio[i].value.toString();
-        }
-        getInfoByCateg(categoryValue);
-        alert(categoryValue);
-    }  */
    currentCategory = categoryForm.categoryRadio.value;
    onGetInfo();
    numberOfPages();
-}
-
-//------------------------------------------------------------
-let tagsForm = <HTMLFormElement>document.getElementById("tagsForm");
-let tagCheckbox = <HTMLInputElement><unknown>document.getElementsByName("tagCheckbox");
-let tagsValue: string;
-
-tagsForm.addEventListener('click', getTags);
-
-function getTags() {
-   // tagsValue = tagsForm.tagCheckbox.value;
-   for (let i=0; i<tagsForm.length; i++) {
-    //   if (tagsForm[i].checked) {
-    //       arrayTags =
-    //   }
-
-   }
-    // alert(tagsValue);
-
 }
 
 
